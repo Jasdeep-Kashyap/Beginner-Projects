93fbeebc3320a4ad88a9af2d45e237e2d0e319b8
Jasdeep-Kashyap <jskkashyap1910@gmail.com>
2026-09-04 16:06:56 +0530

    TypeScript Setup

---
+++ TypeScript/01_project/intro.js
@@ -0,0 +1,35 @@
+"use strict";
+/*const user = {name: "jasdeep", age: 10}
+console.log("Jasdeep")
+console.log(user.name)
+
+let greeting: string = "Jasdeep"
+console.log(greeting)
+*/
+/*Functions*/
+/*
+function addTwo(num: number){
+    return hello
+}                           Still can return string
+addTwo(5)
+
+function addTwo(num: number): number{
+    return hello
+}                            can't return string now
+addTwo(5)
+
+function getUpper(val: string){
+    return val.toUpperCase()
+}
+console.log(getUpper("jasdeep"))*/
+/*
+let loginUser = (name: string, email: string, isPaid: boolean = false) => {}
+
+loginUser("Jasdeep","jasdeep@gmail.com") */
+const getHello = (s) => {
+    return "";
+};
+const heros = ["thor", "spiderman", "ironman"];
+heros.map((hero) => {
+    return `hero is ${hero}`;
+});
---
+++ TypeScript/01_project/intro.ts
@@ -0,0 +1,37 @@
+/*const user = {name: "jasdeep", age: 10}
+console.log("Jasdeep")
+console.log(user.name)
+
+let greeting: string = "Jasdeep"
+console.log(greeting)
+*/
+
+/*Functions*/
+/*
+function addTwo(num: number){
+    return hello
+}                           Still can return string
+addTwo(5)
+
+function addTwo(num: number): number{
+    return hello
+}                            can't return string now
+addTwo(5)
+
+function getUpper(val: string){
+    return val.toUpperCase()
+}
+console.log(getUpper("jasdeep"))*/
+/*
+let loginUser = (name: string, email: string, isPaid: boolean = false) => {}
+
+loginUser("Jasdeep","jasdeep@gmail.com") */
+
+/* const getHello = (s: string):string => {
+    return ""
+}
+const heros = ["thor","spiderman","ironman"]
+heros.map((hero): string => {  /* .map() is used to loop thorugh an array 
+    return `hero is ${hero}`
+})
+*/
